b06182850200279c7ad6e12a032e98af6c1ab575
Mathias WOLFF <mathias@celea.org>
2024-05-30 14:38:38 +0200

    plugin template

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # wazo_bank_holidays
+
+## Installation
+
+```bash
+wazo-plugind-cli -c "install git https://github.com/mwolff44/wazo_bank_holidays"
+```
+
+## Configuration
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import yaml
+
+from setuptools import find_packages
+from setuptools import setup
+
+with open('wazo/plugin.yml') as file:
+    metadata = yaml.load(file)
+
+setup(
+    name=metadata['name'],
+    version=metadata['version'],
+    description=metadata['display_name'],
+    author=metadata['author'],
+    url=metadata['homepage'],
+
+    packages=find_packages()
+)
